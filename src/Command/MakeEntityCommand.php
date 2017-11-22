d824becd973839178d55eb879dc0ce85f9d5d66b
Amrouche Hamza <hamza@les-tilleuls.coop>
2017-11-22 06:48:27 +0100

    [EntityGenerator] add the support of ApiPlatform

--- src/Command/MakeEntityCommand.php
+++ src/Command/MakeEntityCommand.php
@@ -17,6 +17,7 @@ use Symfony\Bundle\MakerBundle\DependencyBuilder;
 use Symfony\Bundle\MakerBundle\Str;
 use Symfony\Bundle\MakerBundle\Validator;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 
 /**
  * @author Javier Eguiluz <javier.eguiluz@gmail.com>
@@ -31,6 +32,7 @@ final class MakeEntityCommand extends AbstractCommand
         $this
             ->setDescription('Creates a new Doctrine entity class')
             ->addArgument('entity-class', InputArgument::OPTIONAL, sprintf('The class name of the entity to create (e.g. <fg=yellow>%s</>)', Str::asClassName(Str::getRandomTerm())))
+            ->addOption('with-api', 'api', InputOption::VALUE_OPTIONAL, sprintf('The class will be used in ApiPlatform.'), false)
             ->setHelp(file_get_contents(__DIR__.'/../Resources/help/MakeEntity.txt'))
         ;
     }
@@ -46,13 +48,14 @@ final class MakeEntityCommand extends AbstractCommand
             'entity_class_name' => $entityClassName,
             'entity_alias' => $entityAlias,
             'repository_class_name' => $repositoryClassName,
+            'api_platform' => $this->input->hasOption('with-api') ? 'Api' : ''
         ];
     }
 
     protected function getFiles(array $params): array
     {
         return [
-            __DIR__.'/../Resources/skeleton/doctrine/Entity.php.txt' => 'src/Entity/'.$params['entity_class_name'].'.php',
+            __DIR__.'/../Resources/skeleton/doctrine/Entity'.$params['api_platform'].'.php.txt' => 'src/Entity/'.$params['entity_class_name'].'.php',
             __DIR__.'/../Resources/skeleton/doctrine/Repository.php.txt' => 'src/Repository/'.$params['repository_class_name'].'.php',
         ];
     }
@@ -72,6 +75,12 @@ final class MakeEntityCommand extends AbstractCommand
 
     protected function configureDependencies(DependencyBuilder $dependencies)
     {
+        if ($this->input->hasOption('with-api') && true === $this->input->getOption('with-api')) {
+           $dependencies->addClassDependency(
+               'ApiPlatform\Core\Annotation\ApiResource',
+               'api'
+           );
+        }
         $dependencies->addClassDependency(
             Column::class,
             'orm'
