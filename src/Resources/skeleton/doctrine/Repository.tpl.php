e7dfff0e95a152242e8fb08b3015710f1244489b
Enleur <enleury@gmail.com>
2018-02-07 18:50:19 +0200

    Add type hints for repository templates

--- src/Resources/skeleton/doctrine/Repository.tpl.php
+++ src/Resources/skeleton/doctrine/Repository.tpl.php
@@ -1,24 +1,30 @@
-<?= "<?php\n" ?>
+<?= "<?php\n"; ?>
 
 namespace App\Repository;
 
-use App\Entity\<?= $entity_class_name ?>;
+use App\Entity\<?= $entity_class_name; ?>;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
-class <?= $repository_class_name ?> extends ServiceEntityRepository
+/**
+ * @method <?= $entity_class_name; ?>|null find($id, $lockMode = null, $lockVersion = null)
+ * @method <?= $entity_class_name; ?>|null findOneBy(array $criteria, array $orderBy = null)
+ * @method <?= $entity_class_name; ?>[]    findAll()
+ * @method <?= $entity_class_name; ?>[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class <?= $repository_class_name; ?> extends ServiceEntityRepository
 {
     public function __construct(RegistryInterface $registry)
     {
-        parent::__construct($registry, <?= $entity_class_name ?>::class);
+        parent::__construct($registry, <?= $entity_class_name; ?>::class);
     }
 
     /*
     public function findBySomething($value)
     {
-        return $this->createQueryBuilder('<?= $entity_alias ?>')
-            ->where('<?= $entity_alias ?>.something = :value')->setParameter('value', $value)
-            ->orderBy('<?= $entity_alias ?>.id', 'ASC')
+        return $this->createQueryBuilder('<?= $entity_alias; ?>')
+            ->where('<?= $entity_alias; ?>.something = :value')->setParameter('value', $value)
+            ->orderBy('<?= $entity_alias; ?>.id', 'ASC')
             ->setMaxResults(10)
             ->getQuery()
             ->getResult()
