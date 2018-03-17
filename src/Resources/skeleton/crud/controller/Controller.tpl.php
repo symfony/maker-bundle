4d1e2d8aa1a1872cb8458abd29d87f10dbd38210
Sadicov Vladimir <sadikoff@gmail.com>
2018-03-17 10:56:06 +0200

    fixing #134

--- src/Resources/skeleton/crud/controller/Controller.tpl.php
+++ src/Resources/skeleton/crud/controller/Controller.tpl.php
@@ -23,7 +23,7 @@ class <?= $class_name ?> extends Controller
 <?php if (isset($repository_full_class_name)): ?>
     public function index(<?= $repository_class_name ?> $<?= $repository_var ?>): Response
     {
-        return $this->render('<?= $route_name ?>/index.html.twig', ['<?= $entity_var_plural ?>' => $<?= $repository_var ?>->findAll()]);
+        return $this->render('<?= $route_name ?>/index.html.twig', ['<?= $entity_twig_var_plural ?>' => $<?= $repository_var ?>->findAll()]);
     }
 <?php else: ?>
     public function index(): Response
