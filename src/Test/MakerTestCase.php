79dc8cede7fab66c57f2048453c158791ee42e26
Sadicov Vladimir <sadikoff@gmail.com>
2018-03-22 14:26:28 +0200

    fix cache if test finishes with exception 2

--- src/Test/MakerTestCase.php
+++ src/Test/MakerTestCase.php
@@ -15,59 +15,54 @@ use PHPUnit\Framework\TestCase;
 
 class MakerTestCase extends TestCase
 {
-    /** @var MakerTestEnvironment */
-    private static $testEnv;
-
     protected function executeMakerCommand(MakerTestDetails $testDetails)
     {
-        self::$testEnv = MakerTestEnvironment::create($testDetails);
+        $testEnv = MakerTestEnvironment::create($testDetails);
 
         // prepare environment to test
-        self::$testEnv->prepare();
+        $testEnv->prepare();
 
-        $makerTestProcess = self::$testEnv->runMaker();
-        //  Run tests
+        try {
+            $makerTestProcess = $testEnv->runMaker();
+            //  Run tests
 
-        $files = self::$testEnv->getGeneratedFilesFromOutputText();
+            $files = $testEnv->getGeneratedFilesFromOutputText();
 
-        foreach ($files as $file) {
-            $this->assertTrue(self::$testEnv->fileExists($file));
+            foreach ($files as $file) {
+                $this->assertTrue($testEnv->fileExists($file));
 
-            if ('.php' == substr($file, -4)) {
-                $csProcess = self::$testEnv->runPhpCSFixer($file);
+                if ('.php' == substr($file, -4)) {
+                    $csProcess = $testEnv->runPhpCSFixer($file);
 
-                $this->assertTrue($csProcess->isSuccessful(), sprintf('File "%s" has a php-cs problem: %s', $file, $csProcess->getOutput()));
+                    $this->assertTrue($csProcess->isSuccessful(), sprintf('File "%s" has a php-cs problem: %s', $file, $csProcess->getOutput()));
+                }
+
+                if ('.twig' == substr($file, -5)) {
+                    $csProcess = $testEnv->runTwigCSLint($file);
+
+                    $this->assertTrue($csProcess->isSuccessful(), sprintf('File "%s" has a twig-cs problem: %s', $file, $csProcess->getOutput()));
+                }
             }
 
-            if ('.twig' == substr($file, -5)) {
-                $csProcess = self::$testEnv->runTwigCSLint($file);
-
-                $this->assertTrue($csProcess->isSuccessful(), sprintf('File "%s" has a twig-cs problem: %s', $file, $csProcess->getOutput()));
+            //run internal tests
+            $internalTestProcess = $testEnv->runInternalTests();
+            if (null !== $internalTestProcess) {
+                $this->assertTrue($internalTestProcess->isSuccessful(), sprintf("Error while running the PHPUnit tests *in* the project: \n\n %s \n\n Command Output: %s", $internalTestProcess->getOutput(), $makerTestProcess->getOutput()));
             }
-        }
 
-        //run internal tests
-        $internalTestProcess = self::$testEnv->runInternalTests();
-        if (null !== $internalTestProcess) {
-            $this->assertTrue($internalTestProcess->isSuccessful(), sprintf("Error while running the PHPUnit tests *in* the project: \n\n %s \n\n Command Output: %s", $internalTestProcess->getOutput(), $makerTestProcess->getOutput()));
-        }
-
-        //checkout user asserts
-        if (null === $testDetails->getAssert()) {
-            $this->assertContains('Success', $makerTestProcess->getOutput(), $makerTestProcess->getErrorOutput());
-        } else {
-            ($testDetails->getAssert())($makerTestProcess->getOutput(), self::$testEnv->getPath());
+            //checkout user asserts
+            if (null === $testDetails->getAssert()) {
+                $this->assertContains('Success', $makerTestProcess->getOutput(), $makerTestProcess->getErrorOutput());
+            } else {
+                ($testDetails->getAssert())($makerTestProcess->getOutput(), $testEnv->getPath());
+            }
+        } catch (\Exception $e) {
+            $testEnv->reset();
+            throw $e;
         }
 
         // reset envirinment
-        self::$testEnv->reset();
-    }
-
-    protected function onNotSuccessfulTest(\Throwable $e)
-    {
-        self::$testEnv->reset();
-
-        throw $e;
+        $testEnv->reset();
     }
 
     protected function assertContainsCount(string $needle, string $haystack, int $count)
