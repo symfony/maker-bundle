4d3cbb1f05ea6a4188e0f8fb0cce04422f6ef8cb
Nicolas Grekas <nicolas.grekas@gmail.com>
2018-04-18 16:05:04 +0200

    Add source links to generated twig template

--- src/Resources/skeleton/controller/twig_template.tpl.php
+++ src/Resources/skeleton/controller/twig_template.tpl.php
@@ -1,18 +1,20 @@
 <?= $helper->getHeadPrintCode('Hello {{ controller_name }}!'); ?>
 
-{% block body %}
+{% block stylesheets %}
 <style>
     .example-wrapper { margin: 1em auto; max-width: 800px; width: 95%; font: 18px/1.5 sans-serif; }
     .example-wrapper code { background: #F5F5F5; padding: 2px 6px; }
 </style>
+{% endblock %}
 
+{% block body %}
 <div class="example-wrapper">
     <h1>Hello {{ controller_name }}! ✅</h1>
 
     This friendly message is coming from:
     <ul>
-        <li>Your controller at <code><?= $controller_path; ?></code></li>
-        <li>Your template at <code><?= $relative_path ?></code></li>
+        <li>Your controller at <code><?= $helper->getFileLink($controller_path); ?></code></li>
+        <li>Your template at <code><?= $helper->getFileLink($relative_path); ?></code></li>
     </ul>
 </div>
 {% endblock %}
