ee3c73fbb76bb51a32a747c97cade86b0f008f88
Honoré HOUNWANOU <mercuryseries@gmail.com>
2018-04-21 10:57:39 -0400

    Decimal Type: Fix small typo
    Scale value currently overwrites the previously entered precision value.

--- src/Maker/MakeEntity.php
+++ src/Maker/MakeEntity.php
@@ -364,7 +364,7 @@ final class MakeEntity extends AbstractMaker
             $data['precision'] = $io->ask('Precision (total number of digits stored: 100.00 would be 5)', 10, [Validator::class, 'validatePrecision']);
 
             // 0 is the default value given in \Doctrine\DBAL\Schema\Column::$_scale
-            $data['precision'] = $io->ask('Scale (number of decimals to store: 100.00 would be 2)', 0, [Validator::class, 'validateScale']);
+            $data['scale'] = $io->ask('Scale (number of decimals to store: 100.00 would be 2)', 0, [Validator::class, 'validateScale']);
         }
 
         if ($io->confirm('Can this field be null in the database (nullable)', false)) {
