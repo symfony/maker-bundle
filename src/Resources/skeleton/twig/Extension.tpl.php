96c4c275add35216d8d3fbbb45c0bdd179556111
Grégoire Pineau <lyrixx@lyrixx.info>
2018-06-13 21:48:59 +0200

    Do not remove escaping on TwigFilter by default

--- src/Resources/skeleton/twig/Extension.tpl.php
+++ src/Resources/skeleton/twig/Extension.tpl.php
@@ -11,7 +11,10 @@ class <?= $class_name ?> extends AbstractExtension
     public function getFilters(): array
     {
         return [
-            new TwigFilter('filter_name', [$this, 'doSomething'], ['is_safe' => ['html']]),
+            // If your filter generates SAFE HTML, you should add a third
+            // parameter: ['is_safe' => ['html']]
+            // Reference: https://twig.symfony.com/doc/2.x/advanced.html#automatic-escaping
+            new TwigFilter('filter_name', [$this, 'doSomething']),
         ];
     }
 
