2337b89b9bb527a4d32caca97dc9bafbfddb4096
Alexey Samara <lion.samara@gmail.com>
2018-10-02 00:33:38 +0300

    Added PHPDoc for class

--- src/Resources/skeleton/authenticator/EmptySecurityController.tpl.php
+++ src/Resources/skeleton/authenticator/EmptySecurityController.tpl.php
@@ -6,6 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\<?= $parent_class_name; ?>;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
+/**
+ * Class <?= $class_name; ?>
+ *
+ * @package App
+ */
 class <?= $class_name; ?> extends <?= $parent_class_name; ?><?= "\n" ?>
 {
 }
