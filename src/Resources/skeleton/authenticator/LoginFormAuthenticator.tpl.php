a5b78e489f6196803351b010908083efa758d602
Alexey Samara <lion.samara@gmail.com>
2018-10-03 17:09:13 +0300

    Added PHPDocs

--- src/Resources/skeleton/authenticator/LoginFormAuthenticator.tpl.php
+++ src/Resources/skeleton/authenticator/LoginFormAuthenticator.tpl.php
@@ -18,15 +18,30 @@ use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
+/**
+ * Class <?= $class_name ?>
+ *
+ * @package <?= $namespace ?>
+ */
 class <?= $class_name; ?> extends AbstractFormLoginAuthenticator
 {
-    use TargetPathTrait;
-
-<?= $user_is_entity ? "    private \$entityManager;\n" : null ?>
+<?= $user_is_entity ? "    /** @var EntityManagerInterface */\n    private \$entityManager;\n\n" : null ?>
+    /** @var RouterInterface */
     private $router;
-    private $csrfTokenManager;
-<?= $user_needs_encoder ? "    private \$passwordEncoder;\n" : null ?>
 
+    /** @var CsrfTokenManagerInterface */
+    private $csrfTokenManager;
+
+<?= $user_needs_encoder ? "    /** @var UserPasswordEncoderInterface */\n    private \$passwordEncoder;\n" : null ?>
+
+    /**
+     * <?= $class_name; ?> constructor.
+     *
+     * @param EntityManagerInterface       $entityManager
+     * @param RouterInterface              $router
+     * @param CsrfTokenManagerInterface    $csrfTokenManager
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     */
     public function __construct(<?= $user_is_entity ? 'EntityManagerInterface $entityManager, ' : null ?>RouterInterface $router, CsrfTokenManagerInterface $csrfTokenManager<?= $user_needs_encoder ? ', UserPasswordEncoderInterface $passwordEncoder' : null ?>)
     {
 <?= $user_is_entity ? "        \$this->entityManager = \$entityManager;\n" : null ?>
@@ -35,12 +50,25 @@ class <?= $class_name; ?> extends AbstractFormLoginAuthenticator
 <?= $user_needs_encoder ? "        \$this->passwordEncoder = \$passwordEncoder;\n" : null ?>
     }
 
+    /**
+     * Does the authenticator support the given Request?
+     *
+     * @param Request $request
+     * @return bool
+     */
     public function supports(Request $request)
     {
         return 'app_login' === $request->attributes->get('_route')
             && $request->isMethod('POST');
     }
 
+    /**
+     * Get the authentication credentials from the request and return them as any type (e.g. an associate array).
+     *
+     * @param Request $request
+     * @return mixed Any non-null value
+     * @throws \UnexpectedValueException If null is returned
+     */
     public function getCredentials(Request $request)
     {
         $credentials = [
@@ -56,6 +84,15 @@ class <?= $class_name; ?> extends AbstractFormLoginAuthenticator
         return $credentials;
     }
 
+    /**
+     * Return a UserInterface object based on the credentials.
+     *
+     * @param mixed                 $credentials
+     * @param UserProviderInterface $userProvider
+     * @return null|UserInterface
+     * @throws \Exception
+     * @throws InvalidCsrfTokenException
+     */
     public function getUser($credentials, UserProviderInterface $userProvider)
     {
         $token = new CsrfToken('authenticate', $credentials['csrf_token']);
@@ -69,6 +106,14 @@ class <?= $class_name; ?> extends AbstractFormLoginAuthenticator
         return \$userProvider->loadUserByUsername(\$credentials['$username_field']);\n"; ?>
     }
 
+    /**
+     * Returns true if the credentials are valid.
+     *
+     * @param mixed         $credentials
+     * @param UserInterface $user
+     * @return bool
+     * @throws AuthenticationException
+     */
     public function checkCredentials($credentials, UserInterface $user)
     {
         <?= $user_needs_encoder ? "return \$this->passwordEncoder->isPasswordValid(\$user, \$credentials['password']);\n"
@@ -77,6 +122,14 @@ class <?= $class_name; ?> extends AbstractFormLoginAuthenticator
         throw new \Exception('TODO: check the credentials inside '.__FILE__);\n" ?>
     }
 
+    /**
+     * Called when authentication executed and was successful!
+     *
+     * @param Request        $request
+     * @param TokenInterface $token
+     * @param string         $providerKey The provider (i.e. firewall) key
+     * @return Response|null
+     */
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
     {
         if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
@@ -87,6 +140,11 @@ class <?= $class_name; ?> extends AbstractFormLoginAuthenticator
         throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
+    /**
+     * Return the URL to the login page.
+     *
+     * @return string
+     */
     protected function getLoginUrl()
     {
         return $this->router->generate('app_login');
