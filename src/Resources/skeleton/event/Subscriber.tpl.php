877113d680d2eb28145af566d48a87a5071c0438
Alexey Samara <lion.samara@gmail.com>
2018-10-04 12:46:50 +0300

    Added PHPDocs

--- src/Resources/skeleton/event/Subscriber.tpl.php
+++ src/Resources/skeleton/event/Subscriber.tpl.php
@@ -5,13 +5,25 @@ namespace <?= $namespace; ?>;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 <?= $event_full_class_name ? "use $event_full_class_name;\n" : '' ?>
 
+/**
+ * Class <?= $class_name ?>
+ * @package <?= $namespace; ?>
+ */
 class <?= $class_name ?> implements EventSubscriberInterface
 {
+    /**
+     * <?= $method_name ?> description
+     *
+     * @param <?= $event_arg ?>
+     */
     public function <?= $method_name ?>(<?= $event_arg ?>)
     {
         // ...
     }
 
+    /**
+     * @return array
+     */
     public static function getSubscribedEvents()
     {
         return [
