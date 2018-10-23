1d39b938ea5c32ebebe6625f34c392b9a6bbbd31
Clemens Krack <info@clemenskrack.com>
2018-10-23 17:09:32 +0200

    Modify make:form to allow non-entities
    Before this modification the make:form command allowed passing a fully specified classname.
    It would generate a form with the correct bound class,  but not read the fields.
    This also affects DTOs.
    This modification makes our lives easier by reading all fields of the bound class.

---
+++ src/Util/ClassDetails.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the Symfony MakerBundle package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Bundle\MakerBundle\Util;
+
+use Symfony\Bundle\MakerBundle\Str;
+
+final class ClassDetails
+{
+    private $fullClassName;
+
+    public function __construct(string $fullClassName)
+    {
+        $this->fullClassName = $fullClassName;
+    }
+
+    public function getProperties():? array
+    {
+        $reflect = new \ReflectionClass($this->fullClassName);
+        $props = $reflect->getProperties();
+
+        $propertiesList = [];
+
+        foreach ($props as $prop) {
+            $propertiesList[] = $prop->getName();
+        }
+
+        return $propertiesList;
+    }
+
+    public function getFormFields()
+    {
+        $properties = $this->getProperties();
+
+        return array_diff($properties,['id']);
+    }
+}
