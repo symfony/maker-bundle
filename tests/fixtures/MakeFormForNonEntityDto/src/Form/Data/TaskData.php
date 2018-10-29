da8f0e04d124d87d4846067224eaba6d068efa3c
Clemens Krack <info@clemenskrack.com>
2018-10-29 22:24:02 +0100

    Simplify DTO and make it PHP 7.0 compatible

--- tests/fixtures/MakeFormForNonEntityDto/src/Form/Data/TaskData.php
+++ tests/fixtures/MakeFormForNonEntityDto/src/Form/Data/TaskData.php
@@ -11,59 +11,12 @@
 
 namespace App\Form\Data;
 
-use App\Entity\Task;
-use Symfony\Component\Validator\Constraints as Assert;
-
 /**
  * Data transfer object for Task.
- * Add your constraints as annotations to the properties.
  */
 class TaskData
 {
-    /**
-     * @Assert\NotBlank()
-     */
     public $task;
 
     public $dueDate;
-
-    /**
-     * Create DTO, optionally extracting data from a model.
-     *
-     * @param Task|null $task
-     */
-    public function __construct(? Task $task = null)
-    {
-        if ($task instanceof Task) {
-            $this->extract($task);
-        }
-    }
-
-    /**
-     * Fill entity with data from the DTO.
-     *
-     * @param Task $task
-     */
-    public function fill(Task $task): Task
-    {
-        $task
-            ->setTask($this->task)
-            ->setDueDate($this->dueDate)
-        ;
-
-        return $task;
-    }
-
-    /**
-     * Extract data from entity into the DTO.
-     *
-     * @param Task $task
-     */
-    public function extract(Task $task): self
-    {
-        $this->task = $task->getTask();
-        $this->dueDate = $task->getDueDate();
-
-        return $this;
-    }
 }
