ff2c90e90c835507b003fc68daa36a9f478d001e
Clemens Krack <info@clemenskrack.com>
2018-11-01 10:39:58 +0100

    Cleanup comments and use PHP_EOL
    Remove useless comments.
    Use PHP_EOL for linebreaks.

--- src/Resources/skeleton/dto/Data.tpl.php
+++ src/Resources/skeleton/dto/Data.tpl.php
@@ -1,4 +1,4 @@
-<?= "<?php\n" ?>
+<?= "<?php" . PHP_EOL ?>
 <?php use Symfony\Bundle\MakerBundle\Str;
 
 ?>
@@ -12,7 +12,6 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * Data transfer object for <?= $bounded_class_name ?>.
- * Add your constraints as annotations to the properties.
  */
 class <?= $class_name ?>
 
@@ -20,11 +19,8 @@ class <?= $class_name ?>
 <?php if ($addHelpers): ?>
     /**
      * Create DTO, optionally extracting data from a model.
-     *
-     * @param <?= $bounded_class_name ?>|null $<?= lcfirst($bounded_class_name) ?>
-
      */
-    public function __construct(<?= $bounded_class_name ?> $<?= lcfirst($bounded_class_name) ?> = null)
+    public function __construct(?<?= $bounded_class_name ?> $<?= lcfirst($bounded_class_name) ?> = null)
     {
         if ($<?= lcfirst($bounded_class_name) ?> instanceof <?= $bounded_class_name ?>) {
             $this->extract($<?= lcfirst($bounded_class_name) ?>);
@@ -33,23 +29,17 @@ class <?= $class_name ?>
 
     /**
      * Fill entity with data from the DTO.
-     *
-     * @param <?= $bounded_class_name ?> $<?= lcfirst($bounded_class_name) ?>
-
      */
-    public function fill(<?= $bounded_class_name ?> $<?= lcfirst($bounded_class_name) ?>): <?= $bounded_class_name ?>
-
+    public function fill(<?= $bounded_class_name ?> $<?= lcfirst($bounded_class_name) ?>): <?= $bounded_class_name ?><?= PHP_EOL ?>
     {
 <?php if (false === $omitGettersSetters): ?>
-        $<?= lcfirst($bounded_class_name) ?>
-
+        $<?= lcfirst($bounded_class_name) ?><?= PHP_EOL ?>
 <?php foreach ($fields as $propertyName => $mapping): ?>
             ->set<?= Str::asCamelCase($propertyName) ?>($this->get<?= Str::asCamelCase($propertyName) ?>())
 <?php endforeach; ?>
         ;
 <?php else : ?>
-        $<?= lcfirst($bounded_class_name) ?>
-
+        $<?= lcfirst($bounded_class_name) ?><?= PHP_EOL ?>
 <?php foreach ($fields as $propertyName => $mapping): ?>
             ->set<?= Str::asCamelCase($propertyName) ?>($this-><?= $propertyName ?>)
 <?php endforeach; ?>
@@ -61,9 +51,6 @@ class <?= $class_name ?>
 
     /**
      * Extract data from entity into the DTO.
-     *
-     * @param <?= $bounded_class_name ?> $<?= lcfirst($bounded_class_name) ?>
-
      */
     public function extract(<?= $bounded_class_name ?> $<?= lcfirst($bounded_class_name) ?>): self
     {
