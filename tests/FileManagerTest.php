3d0f712dc0a44cdc5326ce1effb7ee8761391fdc
LeJeanbono <jean.michel.lec@gmail.com>
2019-01-03 02:02:28 +0100

    Tests templates folder

--- tests/FileManagerTest.php
+++ tests/FileManagerTest.php
@@ -12,9 +12,9 @@ class FileManagerTest extends TestCase
     /**
      * @dataProvider getRelativizePathTests
      */
-    public function testRelativizePath(string $rootDir, string $absolutePath, string $expectedPath)
+    public function testRelativizePath(string $rootDir, string $twigDefaultPath, string $absolutePath, string $expectedPath)
     {
-        $fileManager = new FileManager(new Filesystem(), $this->createMock(AutoloaderUtil::class), $rootDir);
+        $fileManager = new FileManager(new Filesystem(), $this->createMock(AutoloaderUtil::class), $rootDir, $twigDefaultPath);
 
         $this->assertSame($expectedPath, $fileManager->relativizePath($absolutePath));
     }
@@ -23,36 +23,42 @@ class FileManagerTest extends TestCase
     {
         yield [
             '/home/project',
+            'templates',
             '/some/other/path',
             '/some/other/path',
         ];
 
         yield [
             '/home/project',
+            'templates',
             '/home/project/foo/bar',
             'foo/bar',
         ];
 
         yield [
             '/home/project',
+            'templates',
             '/home/project//foo/./bar',
             'foo/bar',
         ];
 
         yield 'relative_dot_path' => [
             '/home/project',
+            'templates',
             '/home/project/foo/bar/../../src/Baz.php',
             'src/Baz.php',
         ];
 
         yield 'windows_path' => [
             'D:\path\to\project',
+            'templates',
             'D:\path\to\project\vendor\composer/../../src/Controller/TestController.php',
             'src/Controller/TestController.php',
         ];
 
         yield 'double_src' => [
             '/src',
+            'templates',
             '/src/vendor/composer/../../src/Command/FooCommand.php',
             'src/Command/FooCommand.php',
         ];
@@ -61,9 +67,9 @@ class FileManagerTest extends TestCase
     /**
      * @dataProvider getAbsolutePathTests
      */
-    public function testAbsolutizePath(string $rootDir, string $path, string $expectedPath)
+    public function testAbsolutizePath(string $rootDir, string $twigDefaultPath, string $path, string $expectedPath)
     {
-        $fileManager = new FileManager(new Filesystem(), $this->createMock(AutoloaderUtil::class), $rootDir);
+        $fileManager = new FileManager(new Filesystem(), $this->createMock(AutoloaderUtil::class), $rootDir, $twigDefaultPath);
         $this->assertSame($expectedPath, $fileManager->absolutizePath($path));
     }
 
@@ -71,24 +77,28 @@ class FileManagerTest extends TestCase
     {
         yield 'normal_path_change' => [
             '/home/project/',
+            'templates',
             'foo/bar',
             '/home/project/foo/bar',
         ];
 
         yield 'already_absolute_path' => [
             '/home/project/',
+            'templates',
             '/foo/bar',
             '/foo/bar',
         ];
 
         yield 'windows_already_absolute_path' => [
             'D:\path\to\project',
+            'templates',
             'D:\foo\bar',
             'D:\foo\bar',
         ];
 
         yield 'windows_already_absolute_path' => [
             'D:\path\to\project',
+            'templates',
             'D:/foo/bar',
             'D:/foo/bar',
         ];
@@ -97,9 +107,9 @@ class FileManagerTest extends TestCase
     /**
      * @dataProvider getIsPathInVendorTests
      */
-    public function testIsPathInVendor(string $rootDir, string $path, bool $expectedIsInVendor)
+    public function testIsPathInVendor(string $rootDir, string $twigDefaultPath, string $path, bool $expectedIsInVendor)
     {
-        $fileManager = new FileManager(new Filesystem(), $this->createMock(AutoloaderUtil::class), $rootDir);
+        $fileManager = new FileManager(new Filesystem(), $this->createMock(AutoloaderUtil::class), $rootDir, $twigDefaultPath);
         $this->assertSame($expectedIsInVendor, $fileManager->isPathInVendor($path));
     }
 
@@ -107,32 +117,55 @@ class FileManagerTest extends TestCase
     {
         yield 'not_in_vendor' => [
             '/home/project/',
+            'templates',
             '/home/project/foo/bar',
             false,
         ];
 
         yield 'in_vendor' => [
             '/home/project/',
+            'templates',
             '/home/project/vendor/foo',
             true,
         ];
 
         yield 'not_in_this_vendor' => [
             '/home/project/',
+            'templates',
             '/other/path/vendor/foo',
             false,
         ];
 
         yield 'windows_not_in_vendor' => [
             'D:\path\to\project',
+            'templates',
             'D:\path\to\project\src\foo',
             false,
         ];
 
         yield 'windows_in_vendor' => [
             'D:\path\to\project',
+            'templates',
             'D:\path\to\project\vendor\foo',
             true,
+        ];
+    }
+
+    /**
+     * @dataProvider getTestTemplatesFolder
+     */
+    public function testTemplatesFolder(string $rootDir, string $twigDefaultPath, string $expectedTemplatesFolder)
+    {
+        $fileManager = new FileManager(new Filesystem(), $this->createMock(AutoloaderUtil::class), $rootDir, $twigDefaultPath);
+        $this->assertSame($expectedTemplatesFolder, $fileManager->getTemplatesFolder());
+    }
+
+    public function getTestTemplatesFolder()
+    {
+        yield 'its_a_folder' => [
+            '/home/project/',
+            'templates',
+            'templates/',
         ];
     }
 }
