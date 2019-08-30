8e15ec942feefda49b62cca982673679424be543
Peter Bowyer <peter@mapledesign.co.uk>
2019-08-30 20:45:48 +0100

    Give getReference the right type hinting
    When using Psalm I need to implement this in every Repository, otherwise an `ArgumentTypeCoercion` Error is reported when I use `$entityManger->getReference()`, as "expects App\Entity\Foo|null, parent type object|null provided".
    
    I'd originally generated my entities and repositories using the Maker bundle, so thought this would be helpful to other developers.

--- src/Resources/skeleton/doctrine/Repository.tpl.php
+++ src/Resources/skeleton/doctrine/Repository.tpl.php
@@ -22,6 +22,19 @@ class <?= $class_name; ?> extends ServiceEntityRepository<?= $with_password_upgr
         parent::__construct($registry, <?= $entity_class_name; ?>::class);
     }
 
+    /**
+     * Obtain a reference to an entity for which the identifier is known, 
+     * without loading that entity from the database.
+     * @see https://www.doctrine-project.org/projects/doctrine-orm/en/2.6/reference/advanced-configuration.html#reference-proxies
+     * @param mixed $id
+     * @return <?= $class_name; ?>
+     * @throws \Doctrine\ORM\ORMException
+     */
+    public function getReference($id)
+    {
+        return $this->getEntityManager()->getReference(<?= $entity_class_name; ?>::class, $id);
+    }
+
 <?php if ($with_password_upgrade): ?>
     /**
      * Used to upgrade (rehash) the user's password automatically over time.
