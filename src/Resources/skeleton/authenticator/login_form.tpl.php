fd5a6d55c7a8108fef9947eabd8400db67435bc3
Thomas Landauer <thomas@landauer.at>
2019-10-26 16:12:35 +0200

    Changed placeholder to real label

--- src/Resources/skeleton/authenticator/login_form.tpl.php
+++ src/Resources/skeleton/authenticator/login_form.tpl.php
@@ -17,10 +17,10 @@
 <?php endif; ?>
 
     <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-    <label for="input<?= ucfirst($username_field); ?>" class="sr-only"><?= $username_label; ?></label>
-    <input type="<?= $username_is_email ? 'email' : 'text'; ?>" value="{{ last_username }}" name="<?= $username_field; ?>" id="input<?= ucfirst($username_field); ?>" class="form-control" placeholder="<?= $username_label; ?>" required autofocus>
-    <label for="inputPassword" class="sr-only">Password</label>
-    <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
+    <label for="input<?= ucfirst($username_field); ?>"><?= $username_label; ?></label>
+    <input type="<?= $username_is_email ? 'email' : 'text'; ?>" value="{{ last_username }}" name="<?= $username_field; ?>" id="input<?= ucfirst($username_field); ?>" class="form-control" required autofocus>
+    <label for="inputPassword">Password</label>
+    <input type="password" name="password" id="inputPassword" class="form-control" required>
 
     <input type="hidden" name="_csrf_token"
            value="{{ csrf_token('authenticate') }}"
