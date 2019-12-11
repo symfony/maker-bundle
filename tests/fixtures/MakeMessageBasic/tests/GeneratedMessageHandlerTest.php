8a2bfbe5024ffea25b186e68eb09f15ca011df92
Nicolas PHILIPPE <nikophil@gmail.com>
2019-12-11 10:08:11 +0100

    fix test for new messenger version

--- tests/fixtures/MakeMessageBasic/tests/GeneratedMessageHandlerTest.php
+++ tests/fixtures/MakeMessageBasic/tests/GeneratedMessageHandlerTest.php
@@ -10,7 +10,7 @@ class GeneratedMessageHandlerTest extends KernelTestCase
     public function testGeneratedMessage()
     {
         self::bootKernel();
-        $messageBus = self::$kernel->getContainer()->get('message_bus');
+        $messageBus = self::$kernel->getContainer()->get('test.message_bus');
 
         $messageBus->dispatch(new SendWelcomeEmail());
         $this->assertTrue(true, 'Smoke testing the handler did not explode');
