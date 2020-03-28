6e4c583302e0cf95332bda39da2ee683068094a7
Jesse Rushlow <jr@geeshoe.com>
2020-03-28 09:32:21 -0400

    remove forgotten password maker
    maker replaced with reset password maker

--- src/Resources/skeleton/forgottenPassword/twig_email.tpl.php
+++
@@ -1,7 +0,0 @@
-Hello,
-
-To reset your password, please visit {{ url('app_reset_password', {tokenAndSelector: token.asString}) }}
-This link will expire in {{ constant('LIFETIME_HOURS', token) }} hours.
-
-Regards,
-the Team.
