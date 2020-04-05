83447aec9bb2d9eaa4c1011b459215b0c3081eda
Jesse Rushlow <jr@geeshoe.com>
2020-04-05 18:19:54 -0400

    Adding email confirmation option to make:registration-form

--- src/Resources/skeleton/registration/twig_template.tpl.php
+++ src/Resources/skeleton/registration/twig_template.tpl.php
@@ -1,11 +1,17 @@
 <?= $helper->getHeadPrintCode('Register'); ?>
 
 {% block body %}
+    {% for flashError in app.flashes('verify_email_error') %}
+        <div class="alert alert-danger" role="alert">{{ flashError }}</div>
+    {% endfor %}
+
     <h1>Register</h1>
 
     {{ form_start(registrationForm) }}
         {{ form_row(registrationForm.<?= $username_field ?>) }}
-        {{ form_row(registrationForm.plainPassword) }}
+        {{ form_row(registrationForm.plainPassword, {
+            label: 'Password'
+        }) }}
         {{ form_row(registrationForm.agreeTerms) }}
 
         <button type="submit" class="btn">Register</button>
