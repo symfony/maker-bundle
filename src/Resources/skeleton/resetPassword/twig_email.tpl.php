a722b2390218f5355d3b242f8da5cf133cb12b55
Pierre du Plessis <pierre@pcservice.co.za>
2020-07-13 10:26:12 +0200

    Re-format email template for reset-password to be friendlier when generating text version

--- src/Resources/skeleton/resetPassword/twig_email.tpl.php
+++ src/Resources/skeleton/resetPassword/twig_email.tpl.php
@@ -1,11 +1,9 @@
 <h1>Hi!</h1>
 
-<p>
-    To reset your password, please visit
-    <a href="{{ url('app_reset_password', {token: resetToken.token}) }}">here</a>
-    This link will expire in {{ tokenLifetime|date('g') }} hour(s)..
-</p>
+<p>To reset your password, please visit the following link</p>
 
-<p>
-    Cheers!
-</p>
+<a href="{{ url('app_reset_password', {token: resetToken.token}) }}">{{ url('app_reset_password', {token: resetToken.token}) }}</a>
+
+<p>This link will expire in {{ tokenLifetime|date('g') }} hour(s).</p>
+
+<p>Cheers!</p>
