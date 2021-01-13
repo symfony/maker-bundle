7c64a6111f267c5b286e585dd10266a9fcc06c91
Grégoire Pineau <lyrixx@lyrixx.info>
2021-01-13 21:06:52 +0100

    Fixed CS

--- src/Util/MakerFileLinkFormatter.php
+++ src/Util/MakerFileLinkFormatter.php
@@ -47,7 +47,7 @@ final class MakerFileLinkFormatter
         }
 
         $outputFormatterStyle = new OutputFormatterStyle();
-        
+
         if (method_exists(OutputFormatterStyle::class, 'setHref')) {
             $outputFormatterStyle->setHref($formatted);
         }
