c1111884ca89900fb531f67679c4e44a234470ac
Jesse Rushlow <jr@rushlow.dev>
2021-02-23 17:12:10 -0500

    add template class detail assertions

--- tests/Util/TemplateClassDetailsTest.php
+++ tests/Util/TemplateClassDetailsTest.php
@@ -24,9 +24,11 @@ class TemplateClassDetailsTest extends TestCase
     {
         $details = new TemplateClassDetails(MakerBundle::class, false);
 
-        self::assertSame('use Symfony\Bundle\MakerBundle\MakerBundle;', $details->getUseStatement());
-        self::assertSame('    private $makerBundle;', $details->getPropertyStatement());
+        self::assertSame(sprintf('use Symfony\Bundle\MakerBundle\MakerBundle;%s', "\n"), $details->getUseStatement());
+        self::assertSame(sprintf('    private $makerBundle;%s', "\n"), $details->getPropertyStatement());
+        self::assertSame(sprintf('        $this->makerBundle = $makerBundle;%s', "\n"), $details->getConstructorArgument());
         self::assertSame('MakerBundle $makerBundle', $details->getMethodArgument());
+        self::assertSame('$this->makerBundle', $details->getProperty());
         self::assertSame('$makerBundle', $details->getVariable());
     }
 
@@ -34,6 +36,31 @@ class TemplateClassDetailsTest extends TestCase
     {
         $details = new TemplateClassDetails(MakerBundle::class, true);
 
-        self::assertSame('private MakerBundle $makerBundle;', $details->getPropertyStatement());
+        self::assertSame(sprintf('    private MakerBundle $makerBundle;%s', "\n"), $details->getPropertyStatement());
+    }
+
+    public function testUseStatementWithoutTrailingNewLine(): void
+    {
+        $details = new TemplateClassDetails(MakerBundle::class, false);
+
+        self::assertSame('use Symfony\Bundle\MakerBundle\MakerBundle;', $details->getUseStatement(false));
+    }
+
+    public function testGetPropertyStatementArguments(): void
+    {
+        $details = new TemplateClassDetails(MakerBundle::class, false);
+
+        self::assertSame('    private $makerBundle;', $details->getPropertyStatement(false));
+        self::assertSame(sprintf('private $makerBundle;%s', "\n"), $details->getPropertyStatement(true, false));
+        self::assertSame('private $makerBundle;', $details->getPropertyStatement(false, false));
+    }
+
+    public function testGetConstructorArgumentArguments(): void
+    {
+        $details = new TemplateClassDetails(MakerBundle::class, false);
+
+        self::assertSame('        $this->makerBundle = $makerBundle;', $details->getConstructorArgument(false, true));
+        self::assertSame(sprintf('$this->makerBundle = $makerBundle;%s', "\n"), $details->getConstructorArgument(true, false));
+        self::assertSame('$this->makerBundle = $makerBundle;', $details->getConstructorArgument(false, false));
     }
 }
