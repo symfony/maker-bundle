8d9f45203937336e1c6f25f6bab9cc3d025cc7f0
Jesse Rushlow <jr@rushlow.dev>
2021-03-29 13:18:16 -0400

    [reset-password] allow anyone to access check email

--- src/Resources/skeleton/resetPassword/ResetPasswordController.tpl.php
+++ src/Resources/skeleton/resetPassword/ResetPasswordController.tpl.php
@@ -75,9 +75,10 @@ class <?= $class_name ?> extends AbstractController
 <?php } ?>
     public function checkEmail(): Response
     {
-        // We prevent users from directly accessing this page
+        // Generate a fake token if the user does not exist or someone hit this page directly.
+        // This prevents exposing whether or not a user was found with the given email address or not
         if (null === ($resetToken = $this->getTokenObjectFromSession())) {
-            return $this->redirectToRoute('app_forgot_password_request');
+            $resetToken = $this->resetPasswordHelper->generateFakeResetToken();
         }
 
         return $this->render('reset_password/check_email.html.twig', [
--- src/Resources/skeleton/resetPassword/twig_check_email.tpl.php
+++ src/Resources/skeleton/resetPassword/twig_check_email.tpl.php
@@ -4,7 +4,7 @@
 
 {% block body %}
     <p>
-        An email has been sent that contains a link that you can click to reset your password.
+        If an account matching your email exists, then an email was just sent that contains a link that you can use to reset your password.
         This link will expire in {{ resetToken.expirationMessageKey|trans(resetToken.expirationMessageData, 'ResetPasswordBundle') }}.
     </p>
     <p>If you don't receive an email please check your spam folder or <a href="{{ path('app_forgot_password_request') }}">try again</a>.</p>
