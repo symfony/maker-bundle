4c57452d5008100aed94f36c8fa698856e480e99
seb-jean <sebastien.jean76@gmail.com>
2021-06-21 11:40:18 +0200

    Update login_form.tpl.php

--- src/Resources/skeleton/authenticator/login_form.tpl.php
+++ src/Resources/skeleton/authenticator/login_form.tpl.php
@@ -11,7 +11,7 @@
 <?php if ($logout_setup): ?>
     {% if app.user %}
         <div class="mb-3">
-            You are logged in as {{ app.user.username }}, <a href="{{ path('app_logout') }}">Logout</a>
+            You are logged in as {{ app.user.userIdentifier }}, <a href="{{ path('app_logout') }}">Logout</a>
         </div>
     {% endif %}
 <?php endif; ?>
