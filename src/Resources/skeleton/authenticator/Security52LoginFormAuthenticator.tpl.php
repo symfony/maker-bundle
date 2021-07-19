a727c5069ebf2697e3594b88ed5ce5e94fccf47b
Jesse Rushlow <jr@rushlow.dev>
2021-07-19 17:00:49 -0400

    remove imported use statements from templates

--- src/Resources/skeleton/authenticator/Security52LoginFormAuthenticator.tpl.php
+++ src/Resources/skeleton/authenticator/Security52LoginFormAuthenticator.tpl.php
@@ -3,19 +3,6 @@
 namespace <?= $namespace ?>;
 
 <?= $use_statements; ?>
-<!--use Symfony\Component\HttpFoundation\RedirectResponse;-->
-<!--use Symfony\Component\HttpFoundation\Request;-->
-<!--use Symfony\Component\HttpFoundation\Response;-->
-<!--use Symfony\Component\Routing\Generator\UrlGeneratorInterface;-->
-<!--use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;-->
-<!--use Symfony\Component\Security\Core\Security;-->
-<!--use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;-->
-<!--use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;-->
-<!--use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;-->
-<!--use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;-->
-<!--use Symfony\Component\Security\Http\Authenticator\Passport\Passport;-->
-<!--use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;-->
-<!--use Symfony\Component\Security\Http\Util\TargetPathTrait;-->
 
 class <?= $class_name; ?> extends AbstractLoginFormAuthenticator
 {
