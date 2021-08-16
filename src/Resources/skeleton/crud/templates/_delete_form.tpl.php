dde74196b0895240810044472740afc4e8b515a3
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-16 23:04:26 +0800

    Delete src directory

--- src/Resources/skeleton/crud/templates/_delete_form.tpl.php
+++
@@ -1,4 +0,0 @@
-<form method="post" action="{{ path('<?= $route_name ?>_delete', {'<?= $entity_identifier ?>': <?= $entity_twig_var_singular ?>.<?= $entity_identifier ?>}) }}" onsubmit="return confirm('Are you sure you want to delete this item?');">
-    <input type="hidden" name="_token" value="{{ csrf_token('delete' ~ <?= $entity_twig_var_singular ?>.<?= $entity_identifier ?>) }}">
-    <button class="btn">Delete</button>
-</form>
--- src/Resources/skeleton/resetPassword/twig_email.tpl.php
+++
@@ -1,9 +0,0 @@
-<h1>Hi!</h1>
-
-<p>To reset your password, please visit the following link</p>
-
-<a href="{{ url('app_reset_password', {token: resetToken.token}) }}">{{ url('app_reset_password', {token: resetToken.token}) }}</a>
-
-<p>This link will expire in {{ resetToken.expirationMessageKey|trans(resetToken.expirationMessageData, 'ResetPasswordBundle') }}.</p>
-
-<p>Cheers!</p>
