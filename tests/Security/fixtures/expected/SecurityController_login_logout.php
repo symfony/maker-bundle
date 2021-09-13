f957ceb0e3f12c0c907c12ecb7853fb12a682584
Jesse Rushlow <jr@rushlow.dev>
2021-09-13 06:25:10 -0400

    add test expectation

--- tests/Security/fixtures/expected/SecurityController_login_logout.php
+++ tests/Security/fixtures/expected/SecurityController_login_logout.php
@@ -29,7 +29,7 @@ class SecurityController extends AbstractController
     /**
      * @Route("/logout", name="app_logout")
      */
-    public function logout()
+    public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
