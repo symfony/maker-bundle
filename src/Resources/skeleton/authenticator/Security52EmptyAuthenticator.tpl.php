1a90d5f2c4ed13fc47ea067450865fb53600302b
Grégoire Pineau <lyrixx@lyrixx.info>
2021-10-06 17:03:14 +0200

    Fixed typo in src/Resources/skeleton/authenticator/Security52EmptyAuthenticator.tpl.php

--- src/Resources/skeleton/authenticator/Security52EmptyAuthenticator.tpl.php
+++ src/Resources/skeleton/authenticator/Security52EmptyAuthenticator.tpl.php
@@ -36,7 +36,7 @@ class <?php echo $class_name ?> extends AbstractAuthenticator
 //        /*
 //         * If you would like this class to control what happens when an anonymous user accesses a
 //         * protected page (e.g. redirect to /login), uncomment this method and make this class
-//         * implement Symfony\Component\Security\Http\EntryPoint\AuthenticationEntrypointInterface.
+//         * implement Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface.
 //         *
 //         * For more details, see https://symfony.com/doc/current/security/experimental_authenticators.html#configuring-the-authentication-entry-point
 //         */
