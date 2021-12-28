5b28174e61111128862887e5a74727382d4755a2
Morgan Abraham <morgan@geekimo.me>
2021-12-28 19:58:21 +0100

    fix: Handle enum as values

--- src/Util/ClassSourceManipulator.php
+++ src/Util/ClassSourceManipulator.php
@@ -512,6 +512,13 @@ final class ClassSourceManipulator
             throw new \Exception('Invalid value: loop before quoting.');
         }
 
+        if (\PHP_VERSION_ID >= 80000) {
+            // do we have an enum ?
+            if (is_object($value) && enum_exists(get_class($value))) {
+                $value = $value->value;
+            }
+        }
+
         return sprintf('"%s"', $value);
     }
 
