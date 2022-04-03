73b5aeef4f4dd76f99027f42006f2ae598ad4031
Hugo Hamon <hugo.hamon@sensiolabs.net>
2022-04-03 16:50:30 +0200

    Add `@extends` tag to Doctrine repository template.

--- tests/Doctrine/fixtures/expected_xml/src/Repository/XOtherRepository.php
+++ tests/Doctrine/fixtures/expected_xml/src/Repository/XOtherRepository.php
@@ -9,6 +9,8 @@ use Doctrine\ORM\ORMException;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
+ * @extends ServiceEntityRepository<XOther>
+ *
  * @method XOther|null find($id, $lockMode = null, $lockVersion = null)
  * @method XOther|null findOneBy(array $criteria, array $orderBy = null)
  * @method XOther[]    findAll()
