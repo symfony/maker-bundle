42369476b6836d7f430413a3764bf2cead89c1dd
Jesse Rushlow <jr@rushlow.dev>
2022-04-25 00:18:36 -0400

    [make:command] use sorted use statements

--- src/Resources/skeleton/command/Command.tpl.php
+++ src/Resources/skeleton/command/Command.tpl.php
@@ -2,17 +2,9 @@
 
 namespace <?= $namespace; ?>;
 
-<?php if ($use_attributes && $use_command_attribute): ?>
-use Symfony\Component\Console\Attribute\AsCommand;
-<?php endif; ?>
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Style\SymfonyStyle;
+<?= $use_statements; ?>
 
-<?php if ($use_attributes && $use_command_attribute): ?>
+<?php if ($use_command_attribute): ?>
 #[AsCommand(
     name: '<?= $command_name; ?>',
     description: 'Add a short description for your command',
@@ -20,7 +12,7 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 <?php endif; ?>
 class <?= $class_name; ?> extends Command
 {
-<?php if (!$use_attributes || !$use_command_attribute): ?>
+<?php if (!$use_command_attribute): ?>
     protected static $defaultName = '<?= $command_name; ?>';
     protected static $defaultDescription = 'Add a short description for your command';
 
