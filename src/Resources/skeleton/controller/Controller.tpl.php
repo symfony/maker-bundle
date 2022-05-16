4aebedf9db14330922ec97477a7df023ac2a7102
Laurent VOULLEMIER <laurent.voullemier@gmail.com>
2022-05-16 11:53:06 +0200

    Return a JsonResponse instead of a Response
    When json method is used in controller.

--- src/Resources/skeleton/controller/Controller.tpl.php
+++ src/Resources/skeleton/controller/Controller.tpl.php
@@ -7,7 +7,8 @@ namespace <?= $namespace; ?>;
 class <?= $class_name; ?> extends AbstractController
 {
 <?= $generator->generateRouteForControllerMethod($route_path, $route_name); ?>
-    public function index(): Response
+    public function index(): <?php if ($with_template) { ?>Response<?php } else { ?>JsonResponse<?php } ?>
+
     {
 <?php if ($with_template) { ?>
         return $this->render('<?= $template_name ?>', [
