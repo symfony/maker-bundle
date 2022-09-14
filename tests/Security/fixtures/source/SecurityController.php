3f013f9083a0ed45b3d875494c5bae6ec42f6c10
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-14 10:57:17 +0800

    Delete tests directory

--- tests/Security/fixtures/source/SecurityController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Controller;
-
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
-class SecurityController extends AbstractController
-{
-}
--- tests/Util/fixtures/add_property/User_empty.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Entity;
-
-class User
-{
-    private $fooProp;
-}
