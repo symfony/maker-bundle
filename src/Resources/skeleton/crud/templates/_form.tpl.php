9467a0350b21493f1f899c682720ef0859604fbc
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-14 10:56:37 +0800

    Delete src directory

--- src/Resources/skeleton/crud/templates/_form.tpl.php
+++
@@ -1,4 +0,0 @@
-{{ form_start(form) }}
-    {{ form_widget(form) }}
-    <button class="btn">{{ button_label|default('Save') }}</button>
-{{ form_end(form) }}
--- src/Resources/skeleton/resetPassword/twig_check_email.tpl.php
+++
@@ -1,8 +0,0 @@
-{% extends 'base.html.twig' %}
-
-{% block title %}Password Reset Email Sent{% endblock %}
-
-{% block body %}
-    <p>An email has been sent that contains a link that you can click to reset your password. This link will expire in {{ tokenLifetime|date('g') }} hour(s).</p>
-    <p>If you don't receive an email please check your spam folder or <a href="{{ path('app_forgot_password_request') }}">try again</a>.</p>
-{% endblock %}
