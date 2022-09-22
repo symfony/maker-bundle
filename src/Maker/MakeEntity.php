4c20d32d02e499561d468540fdbae2143ef25b6f
Yoann Brieux <yoann@brieux.net>
2022-09-22 12:56:46 +0200

    [make:entity] fix compatibility with api-platform 3.0 (#1199)

--- src/Maker/MakeEntity.php
+++ src/Maker/MakeEntity.php
@@ -11,7 +11,8 @@
 
 namespace Symfony\Bundle\MakerBundle\Maker;
 
-use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiResource as LegacyApiResource;
+use ApiPlatform\Metadata\ApiResource;
 use Doctrine\DBAL\Types\Type;
 use Symfony\Bundle\MakerBundle\ConsoleStyle;
 use Symfony\Bundle\MakerBundle\DependencyBuilder;
@@ -133,7 +134,7 @@ final class MakeEntity extends AbstractMaker implements InputAwareMakerInterface
 
         if (
             !$input->getOption('api-resource') &&
-            class_exists(ApiResource::class) &&
+            (class_exists(ApiResource::class) || class_exists(LegacyApiResource::class)) &&
             !class_exists($this->generator->createClassNameDetails($entityClassName, 'Entity\\')->getFullName())
         ) {
             $description = $command->getDefinition()->getOption('api-resource')->getDescription();
@@ -317,10 +318,17 @@ final class MakeEntity extends AbstractMaker implements InputAwareMakerInterface
     public function configureDependencies(DependencyBuilder $dependencies, InputInterface $input = null): void
     {
         if (null !== $input && $input->getOption('api-resource')) {
-            $dependencies->addClassDependency(
-                ApiResource::class,
-                'api'
-            );
+            if (class_exists(ApiResource::class)) {
+                $dependencies->addClassDependency(
+                    ApiResource::class,
+                    'api'
+                );
+            } else {
+                $dependencies->addClassDependency(
+                    LegacyApiResource::class,
+                    'api'
+                );
+            }
         }
 
         if (null !== $input && $input->getOption('broadcast')) {
