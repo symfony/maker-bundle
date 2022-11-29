7831e744fc9d8abd5cda2814161fb61d09f77482
Jesse Rushlow <jr@rushlow.dev>
2022-11-29 02:28:06 -0500

    [make:security:form-login] new maker to use built in FormLogin

---
+++ src/Resources/skeleton/security/formLogin/LoginController.tpl.php
@@ -0,0 +1,23 @@
+<?= "<?php\n" ?>
+
+namespace <?= $namespace; ?>;
+
+<?= $use_statements; ?>
+
+class LoginController extends AbstractController
+{
+    #[Route(path: '/login', name: 'app_login')]
+    public function index(AuthenticationUtils $authenticationUtils): Response
+    {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('login/index.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+}
