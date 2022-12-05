40a380503fdf975dab903e1b3bc419610a319061
Jesse Rushlow <jr@rushlow.dev>
2022-12-05 09:31:41 -0500

    allow user to use their own config and bin

--- src/Util/TemplateLinter.php
+++ src/Util/TemplateLinter.php
@@ -11,6 +11,7 @@
 
 namespace Symfony\Bundle\MakerBundle\Util;
 
+use Symfony\Bundle\MakerBundle\Exception\RuntimeCommandException;
 use Symfony\Component\Process\Process;
 
 /**
@@ -20,21 +21,31 @@ use Symfony\Component\Process\Process;
  */
 final class TemplateLinter
 {
+    public function __construct(
+        private ?string $binaryPath = null,
+        private ?string $configPath = null,
+    ) {
+        if (null === $this->binaryPath) {
+            $this->binaryPath = \dirname(__DIR__).'/Bin/php-cs-fixer-v3.13.0.phar';
+        }
+
+        if (null === $this->configPath) {
+            $this->configPath = \dirname(__DIR__).'/Resources/config/php-cs-fixer.config.php';
+        }
+
+        if (!file_exists($this->binaryPath) || !file_exists($this->configPath)) {
+            throw new RuntimeCommandException('Either the config or binary for PHP_CS_FIXER does not exist.');
+        }
+    }
+
     public function lintPhpTemplate(string|array $templateFilePath): void
     {
         if (\is_string($templateFilePath)) {
             $templateFilePath = [$templateFilePath];
         }
 
-        $fixerPath = \dirname(__DIR__).'/Bin/php-cs-fixer-v3.13.0.phar';
-        $configPath = \dirname(__DIR__).'/Resources/config/php-cs-fixer.config.php';
-
-        if (!file_exists($configPath) || !file_exists($fixerPath)) {
-            throw new \RuntimeException('WTF');
-        }
-
         foreach ($templateFilePath as $filePath) {
-            $process = Process::fromShellCommandline(sprintf('php %s --config=%s --using-cache=no fix %s', $fixerPath, $configPath, $filePath));
+            $process = Process::fromShellCommandline(sprintf('php %s --config=%s --using-cache=no fix %s', $this->binaryPath, $this->configPath, $filePath));
             $process->run();
         }
     }
