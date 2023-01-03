1f98e7b7c6c04f09b0e36b47e213b9ca458075aa
Jesse Rushlow <40327885+jrushlow@users.noreply.github.com>
2023-01-03 01:01:30 -0800

    [make:registration-form] use route attributes for test routes (#1267)

--- tests/fixtures/make-registration-form/standard_setup/src/Controller/TestingController.php
+++ tests/fixtures/make-registration-form/standard_setup/src/Controller/TestingController.php
@@ -4,9 +4,11 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class TestingController extends AbstractController
 {
+    #[Route(path: '/', name: 'app_homepage')]
     public function homepage()
     {
         $this->denyAccessUnlessGranted('ROLE_USER');
@@ -14,6 +16,7 @@ class TestingController extends AbstractController
         return new Response('Page Success');
     }
 
+    #[Route(path: '/anonymous', name: 'app_anonymous')]
     public function anonymous()
     {
         return new Response('Page Success');
