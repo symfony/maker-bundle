53fdabd448b6bcd79ed6a7d47c0dd478fd834235
Florent Morselli <florent.morselli@spomky-labs.com>
2023-12-08 09:42:03 +0100

    Enable last Symfony features for password reset process.

--- src/Resources/skeleton/resetPassword/ChangePasswordFormType.tpl.php
+++ src/Resources/skeleton/resetPassword/ChangePasswordFormType.tpl.php
@@ -22,11 +22,13 @@ class <?= $class_name ?> extends AbstractType
                             'message' => 'Please enter a password',
                         ]),
                         new Length([
-                            'min' => 6,
+                            'min' => 12,
                             'minMessage' => 'Your password should be at least {{ limit }} characters',
                             // max length allowed by Symfony for security reasons
                             'max' => 4096,
                         ]),
+                        new PasswordStrength(),
+                        new NotCompromisedPassword(),
                     ],
                     'label' => 'New password',
                 ],
