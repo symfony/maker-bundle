5abc8c21e1f691d6f2b2b6d7665e5c09d015d4c8
ramity <lewisclbrown@gmail.com>
2024-01-07 18:31:03 +0000

    updated tests comments to match others, added testcase for custom join table

--- tests/Maker/MakeEntityTest.php
+++ tests/Maker/MakeEntityTest.php
@@ -239,7 +239,7 @@ class MakeEntityTest extends MakerTestCase
                     'y',
                     // field name on opposite side - use default 'courses'
                     '',
-                    // skip join table
+                    // join table? - use default 'n'
                     '',
                     // finish adding fields
                     '',
@@ -274,13 +274,44 @@ class MakeEntityTest extends MakerTestCase
                     'y',
                     // field name on opposite side - use default 'courses'
                     '',
-                    // skip join table
+                    // join table? - use default 'n'
                     '',
                     // finish adding fields
                     '',
                 ]);
 
                 $this->runCustomTest($runner, 'it_adds_many_to_many_with_custom_root_namespace.php');
+            }),
+        ];
+
+        yield 'it_adds_many_to_many_with_custom_join_table' => [$this->createMakeEntityTest()
+            ->run(function (MakerTestRunner $runner) {
+                $this->copyEntity($runner, 'User-basic.php');
+
+                $runner->runMaker([
+                    // entity class name
+                    'Course',
+                    // field name
+                    'students',
+                    // add a relationship field
+                    'relation',
+                    // the target entity
+                    'User',
+                    // relation type
+                    'ManyToMany',
+                    // inverse side?
+                    'y',
+                    // field name on opposite side - use default 'courses'
+                    '',
+                    // join table?
+                    'y',
+                    // join table name - use default 'courseStudents'
+                    '',
+                    // finish adding fields
+                    '',
+                ]);
+
+                $this->runCustomTest($runner, 'it_adds_many_to_many_simple.php');
             }),
         ];
 
@@ -367,7 +398,7 @@ class MakeEntityTest extends MakerTestCase
                      * normally, we ask for the field on the *other* side, but we
                      * do not here, since the other side won't be mapped.
                      */
-                    // skip join table
+                    // join table? - use default 'n'
                     '',
                     // finish adding fields
                     '',
