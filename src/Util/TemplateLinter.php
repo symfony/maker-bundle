2a2f0bc20edd1c21144ca52aabaa6e9a9874dfe8
Jesse Rushlow <jr@rushlow.dev>
2024-02-03 01:49:25 -0500

    [make:*] fix bundled php-cs-fixer not working on windows

--- src/Util/TemplateLinter.php
+++ src/Util/TemplateLinter.php
@@ -56,12 +56,21 @@ final class TemplateLinter
             $templateFilePath = [$templateFilePath];
         }
 
+        $ignoreEnv = str_contains(strtolower(\PHP_OS), 'win') ? 'set PHP_CS_FIXER_IGNORE_ENV=1&' : 'PHP_CS_FIXER_IGNORE_ENV=1 ';
+
+        $cmdPrefix = $this->needsPhpCmdPrefix ? 'php ' : '';
+
         foreach ($templateFilePath as $filePath) {
-            $cmdPrefix = $this->needsPhpCmdPrefix ? 'php ' : '';
-
-            $process = Process::fromShellCommandline(sprintf('PHP_CS_FIXER_IGNORE_ENV=1 %s%s --config=%s --using-cache=no fix %s', $cmdPrefix, $this->phpCsFixerBinaryPath, $this->phpCsFixerConfigPath, $filePath));
-
-            $process->run();
+            Process::fromShellCommandline(sprintf(
+                '%s%s%s --config=%s --using-cache=no fix %s',
+                $ignoreEnv,
+                $cmdPrefix,
+                $this->phpCsFixerBinaryPath,
+                $this->phpCsFixerConfigPath,
+                $filePath
+            ))
+                ->run()
+            ;
         }
     }
 
