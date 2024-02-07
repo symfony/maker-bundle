73580f9a89e3513788de9ee07503f17629950105
Jesse Rushlow <40327885+jrushlow@users.noreply.github.com>
2024-02-07 05:32:42 -0500

    feature #1443 [make:controller] deprecate passing PhpCompatUtil to the constructor

--- src/Maker/MakeController.php
+++ src/Maker/MakeController.php
@@ -36,11 +36,13 @@ final class MakeController extends AbstractMaker
 {
     public function __construct(private ?PhpCompatUtil $phpCompatUtil = null)
     {
-        if (null === $phpCompatUtil) {
-            @trigger_error(sprintf('Passing a "%s" instance is mandatory since version 1.42.0', PhpCompatUtil::class), \E_USER_DEPRECATED);
+        if (null !== $phpCompatUtil) {
+            @trigger_deprecation(
+                'symfony/maker-bundle',
+                '1.55.0',
+                sprintf('Initializing MakeCommand while providing an instance of "%s" is deprecated. The $phpCompatUtil param will be removed in a future version.', PhpCompatUtil::class)
+            );
         }
-
-        $this->phpCompatUtil = $phpCompatUtil;
     }
 
     public static function getCommandName(): string
