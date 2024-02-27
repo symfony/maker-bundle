7c25f0bc596254aa79bc222d60cc685d135401c4
Jesse Rushlow <40327885+jrushlow@users.noreply.github.com>
2024-02-27 14:24:34 -0500

    words mean everything
    Co-authored-by: Ryan Weaver <weaverryan+github@gmail.com>

--- tests/fixtures/make-registration-form/expected/RegistrationControllerNoLogin.php
+++ tests/fixtures/make-registration-form/expected/RegistrationControllerNoLogin.php
@@ -21,7 +21,7 @@ class RegistrationController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // encode the plain password
+            // hash the plain password
             $user->setPassword(
                 $userPasswordHasher->hashPassword(
                     $user,
