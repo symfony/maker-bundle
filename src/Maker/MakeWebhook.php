c2775b7310800b744bfe2f36d0a89ec162ae0fd5
Maelan LE BORGNE <maelan.leborgne@sensiolabs.com>
2024-03-25 11:22:26 +0100

    Add new make:webhook command

---
+++ src/Maker/MakeWebhook.php
@@ -0,0 +1,288 @@
+<?php
+
+/*
+ * This file is part of the Symfony MakerBundle package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Bundle\MakerBundle\Maker;
+
+use Symfony\Bundle\MakerBundle\ConsoleStyle;
+use Symfony\Bundle\MakerBundle\DependencyBuilder;
+use Symfony\Bundle\MakerBundle\FileManager;
+use Symfony\Bundle\MakerBundle\Generator;
+use Symfony\Bundle\MakerBundle\InputAwareMakerInterface;
+use Symfony\Bundle\MakerBundle\InputConfiguration;
+use Symfony\Bundle\MakerBundle\Str;
+use Symfony\Bundle\MakerBundle\Util\ClassNameDetails;
+use Symfony\Bundle\MakerBundle\Util\UseStatementGenerator;
+use Symfony\Bundle\MakerBundle\Util\YamlSourceManipulator;
+use Symfony\Bundle\MakerBundle\Validator;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Question\ChoiceQuestion;
+use Symfony\Component\Console\Question\Question;
+use Symfony\Component\ExpressionLanguage\Expression;
+use Symfony\Component\ExpressionLanguage\ExpressionLanguage;
+use Symfony\Component\HttpFoundation\ChainRequestMatcher;
+use Symfony\Component\HttpFoundation\Exception\JsonException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestMatcher\AttributesRequestMatcher;
+use Symfony\Component\HttpFoundation\RequestMatcher\ExpressionRequestMatcher;
+use Symfony\Component\HttpFoundation\RequestMatcher\HostRequestMatcher;
+use Symfony\Component\HttpFoundation\RequestMatcher\IpsRequestMatcher;
+use Symfony\Component\HttpFoundation\RequestMatcher\IsJsonRequestMatcher;
+use Symfony\Component\HttpFoundation\RequestMatcher\MethodRequestMatcher;
+use Symfony\Component\HttpFoundation\RequestMatcher\PathRequestMatcher;
+use Symfony\Component\HttpFoundation\RequestMatcher\PortRequestMatcher;
+use Symfony\Component\HttpFoundation\RequestMatcher\SchemeRequestMatcher;
+use Symfony\Component\HttpFoundation\RequestMatcherInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\RemoteEvent\Consumer\ConsumerInterface;
+use Symfony\Component\RemoteEvent\RemoteEvent;
+use Symfony\Component\Webhook\Client\AbstractRequestParser;
+use Symfony\Component\Webhook\Exception\RejectWebhookException;
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * @author Maelan LE BORGNE <maelan.leborgne@gmail.com>
+ */
+final class MakeWebhook extends AbstractMaker implements InputAwareMakerInterface
+{
+
+    private const WEBHOOK_CONFIG_PATH = 'config/packages/webhook.yaml';
+    private YamlSourceManipulator $ysm;
+
+    public function __construct(
+        private FileManager $fileManager,
+        private Generator $generator,
+    ) {
+    }
+
+    public static function getCommandName(): string
+    {
+        return 'make:webhook';
+    }
+
+    public static function getCommandDescription(): string
+    {
+        return 'Create a new Webhook';
+    }
+
+    public function configureCommand(Command $command, InputConfiguration $inputConfig): void
+    {
+        $command
+            ->addArgument('name', InputArgument::OPTIONAL, sprintf('Name of the webhook to create (e.g. <fg=yellow>github, stripe, ...</>)'))
+            ->setHelp(file_get_contents(__DIR__ . '/../Resources/help/MakeWebhook.txt'))
+        ;
+
+        $inputConfig->setArgumentAsNonInteractive('name');
+    }
+
+    public function configureDependencies(DependencyBuilder $dependencies, ?InputInterface $input = null)
+    {
+        $dependencies->addClassDependency(
+            AbstractRequestParser::class,
+            'webhook'
+        );
+        $dependencies->addClassDependency(
+            ConsumerInterface::class,
+            'remote-event'
+        );
+        $dependencies->addClassDependency(
+            Yaml::class,
+            'yaml'
+        );
+    }
+
+    public function interact(InputInterface $input, ConsoleStyle $io, Command $command): void
+    {
+        if ($input->getArgument('name')) {
+            if (!$this->verifyWebhookName($input->getArgument('name'))) {
+                throw new \InvalidArgumentException('A webhook name can only have alphanumeric characters, underscores, dots, and dashes.');
+            }
+
+            return;
+        }
+
+        $argument = $command->getDefinition()->getArgument('name');
+        $question = new Question($argument->getDescription());
+        $question->setValidator(Validator::notBlank(...));
+        $webhookName = $io->askQuestion($question);
+
+        while (!$this->verifyWebhookName($webhookName)) {
+            $io->error('A webhook name can only have alphanumeric characters, underscores, dots, and dashes.');
+            $webhookName = $io->askQuestion($question);
+        }
+
+        $input->setArgument('name', $webhookName);
+    }
+
+    private function verifyWebhookName(string $entityName): bool
+    {
+        return preg_match('/^[a-zA-Z_\x7f-\xff][a-zA-Z0-9_.\-\x7f-\xff]*$/', $entityName);
+    }
+
+    public function generate(InputInterface $input, ConsoleStyle $io, Generator $generator): void
+    {
+        $webhookName = $input->getArgument('name');
+        $requestParserDetails = $this->generator->createClassNameDetails(
+            Str::asClassName($webhookName . 'RequestParser'),
+            'Webhook\\'
+        );
+        $remoteEventHandlerDetails = $this->generator->createClassNameDetails(
+            Str::asClassName($webhookName . 'WebhookHandler'),
+            'RemoteEvent\\'
+        );
+
+        $this->addToYamlConfig($webhookName, $requestParserDetails);
+
+        $this->generateRequestParser($io, $requestParserDetails);
+
+        $this->generator->generateClass(
+            $remoteEventHandlerDetails->getFullName(),
+            'webhook/WebhookHandler.tpl.php',
+            [
+                'webhook_name' => $webhookName,
+            ]
+        );
+
+        $this->generator->writeChanges();
+        $this->fileManager->dumpFile(self::WEBHOOK_CONFIG_PATH, $this->ysm->getContents());
+    }
+
+    private function addToYamlConfig(string $webhookName, ClassNameDetails $requestParserDetails): void
+    {
+        if (!$this->fileManager->fileExists(self::WEBHOOK_CONFIG_PATH)) {
+            $yamlConfig = Yaml::dump(['framework' => ['webhook' => ['routing' => []]]], 4, 2);
+        } else {
+            $yamlConfig = $this->fileManager->getFileContents(self::WEBHOOK_CONFIG_PATH);
+        }
+        $this->ysm = new YamlSourceManipulator($yamlConfig);
+        $arrayConfig = $this->ysm->getData();
+        if (key_exists($webhookName, $arrayConfig['framework']['webhook']['routing'] ?? [])) {
+            throw new \InvalidArgumentException('A webhook with this name already exists');
+        }
+
+        $arrayConfig['framework']['webhook']['routing'][$webhookName] = [
+            'service' => $requestParserDetails->getFullName(),
+            'secret' => 'your_secret_here',
+        ];
+        $this->ysm->setData(
+            $arrayConfig
+        );
+    }
+
+    /**
+     * @param ConsoleStyle $io
+     * @param ClassNameDetails $requestParserDetails
+     * @return void
+     * @throws \Exception
+     */
+    public function generateRequestParser(ConsoleStyle $io, ClassNameDetails $requestParserDetails): void
+    {
+        $useStatements = new UseStatementGenerator([
+            JsonException::class,
+            Request::class,
+            Response::class,
+            RemoteEvent::class,
+            AbstractRequestParser::class,
+            RejectWebhookException::class,
+            RequestMatcherInterface::class,
+        ]);
+        $requestMatchers = [];
+        while (true) {
+            $newRequestMatcher = $this->askForNextRequestMatcher($io, $requestMatchers, $requestParserDetails->getFullName(), empty($requestMatchers));
+            if (null === $newRequestMatcher) {
+                break;
+            }
+            $requestMatchers[] = $newRequestMatcher;
+        }
+        $useChainRequestsMatcher = false;
+        // Use a ChainRequestMatcher if multiple matchers have been added OR if none (will be printed with an empty array)
+        if (1 !== \count($requestMatchers)) {
+            $useChainRequestsMatcher = true;
+            $useStatements->addUseStatement(ChainRequestMatcher::class);
+        }
+        $requestMatcherArguments = [];
+        foreach ($requestMatchers as $requestMatcherClass) {
+            $useStatements->addUseStatement($requestMatcherClass);
+            $requestMatcherArguments[$requestMatcherClass] = $this->getRequestMatcherArguments($requestMatcherClass);
+            if (ExpressionRequestMatcher::class === $requestMatcherClass) {
+                if (!class_exists(Expression::class)) {
+                    throw new \Exception('The ExpressionRequestMatcher requires the symfony/expression-language package.');
+                }
+                $useStatements->addUseStatement(Expression::class);
+                $useStatements->addUseStatement(ExpressionLanguage::class);
+            }
+        }
+
+        $this->generator->generateClass(
+            $requestParserDetails->getFullName(),
+            'webhook/RequestParser.tpl.php',
+            [
+                'use_statements' => $useStatements,
+                'use_chained_requests_matcher' => $useChainRequestsMatcher,
+                'request_matchers' => $requestMatchers,
+                'request_matcher_arguments' => $requestMatcherArguments,
+            ]
+        );
+    }
+
+    private function askForNextRequestMatcher(ConsoleStyle $io, array $addedMatchers, string $entityClass, bool $isFirstMatcher): string|null
+    {
+        $io->writeln('');
+        $availableMatchers = $this->getAvailableRequestMatchers();
+        $matcherName = null;
+        while (null === $matcherName) {
+            if ($isFirstMatcher) {
+                $questionText = 'Add a RequestMatcher (press <return> to skip this step)';
+            } else {
+                $questionText = 'Add another RequestMatcher? Enter the RequestMatcher name (or press <return> to stop adding matchers)';
+            }
+
+            $choices = array_diff($availableMatchers, $addedMatchers);
+            $question = new ChoiceQuestion($questionText, array_values(['<skip>'] + $choices), 0);
+            $matcherName = $io->askQuestion($question);
+            if ('<skip>' === $matcherName) {
+                return null;
+            }
+        }
+        return $matcherName;
+    }
+
+    private function getAvailableRequestMatchers(): array
+    {
+        return [
+            AttributesRequestMatcher::class,
+            ExpressionRequestMatcher::class,
+            HostRequestMatcher::class,
+            IpsRequestMatcher::class,
+            IsJsonRequestMatcher::class,
+            MethodRequestMatcher::class,
+            PathRequestMatcher::class,
+            PortRequestMatcher::class,
+            SchemeRequestMatcher::class,
+        ];
+    }
+
+    private function getRequestMatcherArguments(string $requestMatcherClass)
+    {
+        return match ($requestMatcherClass) {
+            AttributesRequestMatcher::class => '[\'attributeName\' => \'regex\']',
+            ExpressionRequestMatcher::class => 'new ExpressionLanguage(), new Expression(\'expression\')',
+            HostRequestMatcher::class, PathRequestMatcher::class => '\'regex\'',
+            IpsRequestMatcher::class => '[\'127.0.0.1\']',
+            IsJsonRequestMatcher::class => '',
+            MethodRequestMatcher::class => '\'POST\'',
+            PortRequestMatcher::class => '443',
+            SchemeRequestMatcher::class => 'https',
+            default => '[]',
+        };
+    }
+}
---
+++ tests/Maker/MakeWebhookTest.php
@@ -0,0 +1,203 @@
+<?php
+
+/*
+ * This file is part of the Symfony MakerBundle package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Bundle\MakerBundle\Tests\Maker;
+
+use Symfony\Bundle\MakerBundle\Maker\MakeWebhook;
+use Symfony\Bundle\MakerBundle\Test\MakerTestCase;
+use Symfony\Bundle\MakerBundle\Test\MakerTestRunner;
+
+class MakeWebhookTest extends MakerTestCase
+{
+    protected function getMakerClass(): string
+    {
+        return MakeWebhook::class;
+    }
+
+    public function getTestDetails(): \Generator
+    {
+        yield 'it_makes_webhook_whit_no_prior_config_file' => [$this->createMakerTest()
+            ->run(function (MakerTestRunner $runner) {
+                $output = $runner->runMaker([
+                    // webhook name
+                    'remote_service',
+                    // skip adding matchers
+                    '',
+                ]);
+                $this->assertStringContainsString('created:', $output);
+                $this->assertFileExists($runner->getPath('src/Webhook/RemoteServiceRequestParser.php'));
+                $this->assertStringContainsString(
+                    'use Symfony\Component\Webhook\Client\AbstractRequestParser;',
+                    file_get_contents($runner->getPath('src/Webhook/RemoteServiceRequestParser.php'))
+                );
+                $this->assertFileExists($runner->getPath('src/RemoteEvent/RemoteServiceWebhookHandler.php'));
+                $this->assertStringContainsString(
+                    '#[AsRemoteEventConsumer(\'remote_service\')]',
+                    file_get_contents($runner->getPath('src/RemoteEvent/RemoteServiceWebhookHandler.php')),
+                );
+                $securityConfig = $runner->readYaml('config/packages/webhook.yaml');
+                $this->assertEquals(
+                    'App\\Webhook\\RemoteServiceRequestParser',
+                    $securityConfig['framework']['webhook']['routing']['remote_service']['service']
+                );
+                $this->assertEquals(
+                    'your_secret_here',
+                    $securityConfig['framework']['webhook']['routing']['remote_service']['secret']
+                );
+            }),
+        ];
+
+        yield 'it_makes_webhook_with_prior_webhook' => [$this->createMakerTest()
+            ->run(function (MakerTestRunner $runner) {
+                $runner->copy('make-webhook/webhook.yaml', 'config/packages/webhook.yaml');
+                $runner->copy('make-webhook/RemoteServiceRequestParser.php', 'src/Webhook/RemoteServiceRequestParser.php');
+                $runner->copy('make-webhook/RemoteServiceWebhookHandler.php', 'src/RemoteEvent/RemoteServiceWebhookHandler.php');
+                $output = $runner->runMaker([
+                    // webhook name
+                    'another_remote_service',
+                    // skip adding matchers
+                    '',
+                ]);
+                $this->assertStringContainsString('created:', $output);
+                $this->assertFileExists($runner->getPath('src/Webhook/AnotherRemoteServiceRequestParser.php'));
+                $this->assertFileExists($runner->getPath('src/RemoteEvent/AnotherRemoteServiceWebhookHandler.php'));
+                $securityConfig = $runner->readYaml('config/packages/webhook.yaml');
+                // original config should not be modified
+                $this->assertArrayHasKey('remote_service', $securityConfig['framework']['webhook']['routing']);
+                $this->assertEquals(
+                    'App\\Webhook\\RemoteServiceRequestParser',
+                    $securityConfig['framework']['webhook']['routing']['remote_service']['service']
+                );
+                $this->assertEquals(
+                    '%env(REMOTE_SERVICE_WEBHOOK_SECRET)%',
+                    $securityConfig['framework']['webhook']['routing']['remote_service']['secret']
+                );
+                // new config should be added
+                $this->assertArrayHasKey('another_remote_service', $securityConfig['framework']['webhook']['routing']);
+                $this->assertEquals(
+                    'App\\Webhook\\AnotherRemoteServiceRequestParser',
+                    $securityConfig['framework']['webhook']['routing']['another_remote_service']['service']
+                );
+                $this->assertEquals(
+                    'your_secret_here',
+                    $securityConfig['framework']['webhook']['routing']['another_remote_service']['secret']
+                );
+            }),
+        ];
+
+        yield 'it_makes_webhook_with_single_matcher' => [$this->createMakerTest()
+            ->run(function (MakerTestRunner $runner) {
+                $output = $runner->runMaker([
+                    // webhook name
+                    'remote_service',
+                    // add a matcher
+                    'Symfony\Component\HttpFoundation\RequestMatcher\IsJsonRequestMatcher',
+                ]);
+                $this->assertStringContainsString('created:', $output);
+                $this->assertFileExists($runner->getPath('src/Webhook/RemoteServiceRequestParser.php'));
+                $this->assertFileExists($runner->getPath('src/RemoteEvent/RemoteServiceWebhookHandler.php'));
+                $requestParserSource = file_get_contents($runner->getPath('src/Webhook/RemoteServiceRequestParser.php'));
+                $this->assertStringContainsString(
+                    'use Symfony\Component\HttpFoundation\RequestMatcher\IsJsonRequestMatcher;',
+                    $requestParserSource
+                );
+                $this->assertStringContainsString(
+                    'return new IsJsonRequestMatcher();',
+                    $requestParserSource
+                );
+            }),
+        ];
+
+        yield 'it_makes_webhook_with_multiple_matchers' => [$this->createMakerTest()
+            ->run(function (MakerTestRunner $runner) {
+                $output = $runner->runMaker([
+                    // webhook name
+                    'remote_service',
+                    // add matchers
+                    'Symfony\Component\HttpFoundation\RequestMatcher\IsJsonRequestMatcher',
+                    'Symfony\Component\HttpFoundation\RequestMatcher\PortRequestMatcher',
+                ]);
+                $this->assertStringContainsString('created:', $output);
+                $this->assertFileExists($runner->getPath('src/Webhook/RemoteServiceRequestParser.php'));
+                $this->assertFileExists($runner->getPath('src/RemoteEvent/RemoteServiceWebhookHandler.php'));
+                $requestParserSource = file_get_contents($runner->getPath('src/Webhook/RemoteServiceRequestParser.php'));
+                $this->assertStringContainsString(
+                    'use Symfony\Component\HttpFoundation\RequestMatcher\IsJsonRequestMatcher;',
+                    $requestParserSource
+                );
+                $this->assertStringContainsString(
+                    'use Symfony\Component\HttpFoundation\RequestMatcher\PortRequestMatcher;',
+                    $requestParserSource
+                );
+                $this->assertStringContainsString(
+                    'use Symfony\Component\HttpFoundation\ChainRequestMatcher;',
+                    $requestParserSource
+                );
+                $this->assertStringContainsString(
+                    <<<EOF
+                            return new ChainRequestMatcher([
+                                new IsJsonRequestMatcher(),
+                                new PortRequestMatcher(443),
+                            ]);
+                    EOF,
+                    $requestParserSource
+                );
+            }),
+        ];
+
+        yield 'it_makes_webhook_with_expression_language_injection' => [$this->createMakerTest()
+            ->addExtraDependencies('symfony/expression-language')
+            ->run(function (MakerTestRunner $runner) {
+
+                $output = $runner->runMaker([
+                    // webhook name
+                    'remote_service',
+                    // add matchers
+                    'Symfony\Component\HttpFoundation\RequestMatcher\IsJsonRequestMatcher',
+                    'Symfony\Component\HttpFoundation\RequestMatcher\ExpressionRequestMatcher',
+                ]);
+                $this->assertStringContainsString('created:', $output);
+                $this->assertFileExists($runner->getPath('src/Webhook/RemoteServiceRequestParser.php'));
+                $this->assertFileExists($runner->getPath('src/RemoteEvent/RemoteServiceWebhookHandler.php'));
+                $requestParserSource = file_get_contents($runner->getPath('src/Webhook/RemoteServiceRequestParser.php'));
+                $this->assertStringContainsString(
+                    'use Symfony\Component\HttpFoundation\RequestMatcher\IsJsonRequestMatcher;',
+                    $requestParserSource
+                );
+                $this->assertStringContainsString(
+                    'use Symfony\Component\HttpFoundation\RequestMatcher\ExpressionRequestMatcher;',
+                    $requestParserSource
+                );
+                $this->assertStringContainsString(
+                    'use Symfony\Component\HttpFoundation\ChainRequestMatcher;',
+                    $requestParserSource
+                );
+                $this->assertStringContainsString(
+                    'use Symfony\Component\ExpressionLanguage\Expression;',
+                    $requestParserSource
+                );
+                $this->assertStringContainsString(
+                    'use Symfony\Component\ExpressionLanguage\ExpressionLanguage;',
+                    $requestParserSource
+                );
+                $this->assertStringContainsString(
+                    <<<EOF
+                            return new ChainRequestMatcher([
+                                new IsJsonRequestMatcher(),
+                                new ExpressionRequestMatcher(new ExpressionLanguage(), new Expression('expression')),
+                            ]);
+                    EOF,
+                    $requestParserSource
+                );
+            }),
+        ];
+    }
+}
