9cde4bee887f812d53b9636f94afb932250c4918
Jesse Rushlow <jr@rushlow.dev>
2024-04-17 06:21:58 -0400

    php-cs-fixer it up

--- src/MakerBundle.php
+++ src/MakerBundle.php
@@ -19,7 +19,6 @@ use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
-use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
  * @author Javier Eguiluz <javier.eguiluz@gmail.com>
