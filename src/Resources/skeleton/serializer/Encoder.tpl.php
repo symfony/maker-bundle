7863663a5a9da5c2713a36f549035547ad584a8a
Jesse Rushlow <jr@rushlow.dev>
2024-04-24 04:42:12 -0400

    [make:serializer:encoder] fix interface signature mismatch in template

--- src/Resources/skeleton/serializer/Encoder.tpl.php
+++ src/Resources/skeleton/serializer/Encoder.tpl.php
@@ -8,24 +8,24 @@ class <?= $class_name ?> implements EncoderInterface, DecoderInterface
 {
     public const FORMAT = '<?= $format ?>';
 
-    public function encode($data, string $format, array $context = []): string
+    public function encode(mixed $data, string $format, array $context = []): string
     {
         // TODO: return your encoded data
         return '';
     }
 
-    public function supportsEncoding(string $format, array $context = []): bool
+    public function supportsEncoding(string $format): bool
     {
         return self::FORMAT === $format;
     }
 
-    public function decode(string $data, string $format, array $context = [])
+    public function decode(string $data, string $format, array $context = [])<?php if ($use_decoder_return_type): ?>: mixed<?php endif; ?>
     {
         // TODO: return your decoded data
         return '';
     }
 
-    public function supportsDecoding(string $format, array $context = []): bool
+    public function supportsDecoding(string $format): bool
     {
         return self::FORMAT === $format;
     }
