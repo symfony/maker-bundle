815075a420c31ef50bf3f08de6c21e79b16d91fe
shadowc <info@matiasjose.com>
2024-06-17 22:10:35 +0200

    Remove unused import

--- src/Maker/MakeTwigComponent.php
+++ src/Maker/MakeTwigComponent.php
@@ -20,7 +20,6 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Yaml\Exception\ParseException;
 use Symfony\Component\Yaml\Yaml;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
