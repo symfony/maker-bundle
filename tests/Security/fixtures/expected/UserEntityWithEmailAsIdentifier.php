2d1007f5a59aa18d4a53eab33a71bed3601920a1
seb-jean <sebastien.jean76@gmail.com>
2024-06-17 18:44:32 +0200

    bug #1550 [make:user] fix `getPassword()` return type in certain instance with `PasswordAuthenticatedUserInterface`
    * Update UserClassBuilder.php
    
    * fix nullable return type
    
    ---------
    
    Co-authored-by: Jesse Rushlow <jr@rushlow.dev>

--- tests/Security/fixtures/expected/UserEntityWithEmailAsIdentifier.php
+++ tests/Security/fixtures/expected/UserEntityWithEmailAsIdentifier.php
@@ -83,7 +83,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     /**
      * @see PasswordAuthenticatedUserInterface
      */
-    public function getPassword(): string
+    public function getPassword(): ?string
     {
         return $this->password;
     }
