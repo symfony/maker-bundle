b8707f16ac46a2e9c93e35cdb5fd69c1341c6067
seb-jean <sebastien.jean76@gmail.com>
2024-06-17 14:10:18 +0200

    feature #1549 [make:registration-form] improve generated types for phpstan
    * Update EmailVerifier.tpl.php
    
    * Update RegistrationController.tpl.php
    
    * fix expected templates to match generated changes
    
    ---------
    
    Co-authored-by: Jesse Rushlow <jr@rushlow.dev>

--- tests/fixtures/make-registration-form/expected/RegistrationControllerCustomAuthenticator.php
+++ tests/fixtures/make-registration-form/expected/RegistrationControllerCustomAuthenticator.php
@@ -23,13 +23,11 @@ class RegistrationController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            /** @var string $plainPassword */
+            $plainPassword = $form->get('plainPassword')->getData();
+
             // encode the plain password
-            $user->setPassword(
-                $userPasswordHasher->hashPassword(
-                    $user,
-                    $form->get('plainPassword')->getData()
-                )
-            );
+            $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
 
             $entityManager->persist($user);
             $entityManager->flush();
