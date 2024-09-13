bca976079d81f334247b33c3a6d044c3ff691138
Jesse Rushlow <jr@rushlow.dev>
2024-09-13 07:41:02 -0400

    match expectations created in #1590

--- tests/fixtures/make-validator/expected/FooBar.php
+++ tests/fixtures/make-validator/expected/FooBar.php
@@ -4,16 +4,18 @@ namespace App\Validator;
 
 use Symfony\Component\Validator\Constraint;
 
-/**
- * @Annotation
- * @Target({"PROPERTY", "METHOD", "ANNOTATION"})
- */
 #[\Attribute(\Attribute::TARGET_PROPERTY | \Attribute::TARGET_METHOD | \Attribute::IS_REPEATABLE)]
 final class FooBar extends Constraint
 {
-    /*
-     * Any public properties become valid options for the annotation.
-     * Then, use these in your validator class.
-     */
-    public string $message = 'The value "{{ value }}" is not valid.';
+    public string $message = 'The string "{{ string }}" contains an illegal character: it can only contain letters or numbers.';
+
+    // You can use #[HasNamedArguments] to make some constraint options required.
+    // All configurable options must be passed to the constructor.
+    public function __construct(
+        public string $mode = 'strict',
+        ?array $groups = null,
+        mixed $payload = null
+    ) {
+        parent::__construct([], $groups, $payload);
+    }
 }
