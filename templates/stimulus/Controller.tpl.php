14570ee82930d8cf3a357fc4c3f16bff8591a50b
Simon André <smn.andre@gmail.com>
2024-12-25 15:17:45 +0100

    Add methods initialize, connect and disconnect with code comment

--- templates/stimulus/Controller.tpl.php
+++ templates/stimulus/Controller.tpl.php
@@ -14,5 +14,32 @@ export default class extends Controller {
 <?php endforeach; ?>
     }
 <?php } ?>
-    // ...
+
+    initialize() {
+        // Called once when the controller is first instantiated (per element)
+
+        // Here you can initialize variables, create scoped callables for event
+        // listeners, instantiate external libraries, etc.
+        // this._fooBar = this.fooBar.bind(this)
+    }
+
+    connect() {
+        // Called every time the controller is connected to the DOM
+        // (on page load, when it's added to the DOM, moved in the DOM, etc.)
+
+        // Here you can add event listeners on the element or target elements,
+        // add or remove classes, attributes, dispatch custom events, etc.
+        // this.fooTarget.addEventListener('click', this._fooBar)
+    }
+
+    // Add custom controller actions here
+    // fooBar() { this.fooTarget.classList.toggle(this.bazClass) }
+
+    disconnect() {
+        // Called anytime its element is disconnected from the DOM
+        // (on page change, when it's removed from or moved in the DOM, etc.)
+
+        // Here you should remove all event listeners added in "connect()" 
+        // this.fooTarget.removeEventListener('click', this._fooBar)
+    }
 }
