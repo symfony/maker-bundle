49d28a66e41860761fde18a22aac81e77191d7e6
Simon André <smn.andre@gmail.com>
2024-12-25 15:19:25 +0100

    Add line break before "stimulusFetch: lazy" to emphasis line and help UX parsers

--- templates/stimulus/Controller.tpl.php
+++ templates/stimulus/Controller.tpl.php
@@ -4,6 +4,7 @@ import { Controller } from '@hotwired/stimulus';
 * The following line makes this controller "lazy": it won't be downloaded until needed
 * See https://symfony.com/bundles/StimulusBundle/current/index.html#lazy-stimulus-controllers
 */
+
 /* stimulusFetch: 'lazy' */
 export default class extends Controller {
 <?= $targets ? "    static targets = $targets\n" : "" ?>
