9915c2f2419a2120677175e277fe52c055c6f6d5
Simon André <smn.andre@gmail.com>
2024-12-25 15:24:49 +0100

    Update tests for lazy doc changes

--- tests/fixtures/make-stimulus-controller/without_targets.js
+++ tests/fixtures/make-stimulus-controller/without_targets.js
@@ -2,7 +2,7 @@ import { Controller } from '@hotwired/stimulus';
 
 /*
 * The following line makes this controller "lazy": it won't be downloaded until needed
-* See https://github.com/symfony/stimulus-bridge#lazy-controllers
+* See https://symfony.com/bundles/StimulusBundle/current/index.html#lazy-stimulus-controllers
 */
 /* stimulusFetch: 'lazy' */
 export default class extends Controller {
