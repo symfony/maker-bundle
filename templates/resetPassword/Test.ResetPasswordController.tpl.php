40c9950c1841d5a6605e46de933d6aa816778805
Florent Destremau <florentdestremau@users.noreply.github.com>
2025-03-05 21:27:50 +0100

    Fix typo

--- templates/resetPassword/Test.ResetPasswordController.tpl.php
+++ templates/resetPassword/Test.ResetPasswordController.tpl.php
@@ -72,7 +72,7 @@ class ResetPasswordControllerTest extends WebTestCase
         // Test the link sent in the email is valid
         /** @var array<TemplatedEmail> $messages */
         $email = $messages[0]->getTextBody();
-        self::assertIsString($emailBody);
+        self::assertIsString($email);
         preg_match('#(/reset-password/reset/[a-zA-Z0-9]+)#', $email, $resetLink);
 
         $this->client->request('GET', $resetLink[1]);
