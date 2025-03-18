0f1c926c0e226e3bca15f77f8df1ef55fc44ec3a
pierrebaudoin <pierre@novalend.com>
2025-03-18 16:39:25 +0100

    fix: default validator param message

--- tests/fixtures/make-validator/expected/FooBar.php
+++ tests/fixtures/make-validator/expected/FooBar.php
@@ -7,7 +7,7 @@ use Symfony\Component\Validator\Constraint;
 #[\Attribute(\Attribute::TARGET_PROPERTY | \Attribute::TARGET_METHOD | \Attribute::IS_REPEATABLE)]
 final class FooBar extends Constraint
 {
-    public string $message = 'The string "{{ string }}" contains an illegal character: it can only contain letters or numbers.';
+    public string $message = 'The string "{{ value }}" contains an illegal character: it can only contain letters or numbers.';
 
     // You can use #[HasNamedArguments] to make some constraint options required.
     // All configurable options must be passed to the constructor.
