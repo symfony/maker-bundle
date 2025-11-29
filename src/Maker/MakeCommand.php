108abf818ebbfbdbf49cb78b7b5faa4019031c38
Kevin Jansen <kevitius99@gmail.com>
2025-11-29 12:45:23 +0100

    Add a failsafe on option retrieval for invokable

--- src/Maker/MakeCommand.php
+++ src/Maker/MakeCommand.php
@@ -81,7 +81,7 @@ final class MakeCommand extends AbstractMaker
             \sprintf('The "%s" command name is not valid because it would be implemented by "%s" class, which is not valid as a PHP class name (it must start with a letter or underscore, followed by any number of letters, numbers, or underscores).', $commandName, Str::asClassName($commandName, 'Command'))
         );
 
-        $input->getOption('invokable') ?
+        $this->supportsInvokableCommand() && $input->getOption('invokable') ?
             $this->generateInvokableCommand($commandName, $commandClassNameDetails, $io, $generator) :
             $this->generateInheritanceCommand($commandName, $commandClassNameDetails, $io, $generator);
     }
